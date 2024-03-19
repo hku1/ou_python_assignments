56ee1d0c9cbdb635d800079a3c1f963d9e0662ca
Saul-76 <er_is_geen_naam_meer_vrij@hotmail.com>
2024-03-19 20:59:58 +0100

    "Functie make string toegevoegd aan Planner"

--- assignment_1/planner.py
+++ assignment_1/planner.py
@@ -1,4 +1,4 @@
-import course
+    import course
 
 
 class Planner:
@@ -42,41 +42,52 @@ class Planner:
        :return: string for this quartile
        """
 
+        result = ''
+        for i in range(1, 5):
+            self.make_string(self.choose_course(i))
+        return result
+
+
     # TODO: implement
 
     def generate(self):
         """
         :return: string showing the planning
         """
-        result = ''
-        for i in range(1, 5):
-            result += self.generate_for_quartile(i)
-        return result
+        self.generate_for_quartile(1)
 
     # Check below for errors.
     # Check for comparison of done_codes and available_courses
 
     def bepaal_cursus_nog_niet_gedaan(self) -> list[course]:
-        """Gives the courses which are not done yet"""
+        """Gives the courses which are not done yet
+            return: list of courses not done yet
+        """
         return_list = list(set(self.preparation.available_courses) - set(self.preparation.done_codes))
         return return_list
 
     def bepaal_cursus_vereiste_voorkennis(self) -> list[course]:
-        """Gives the mandatory courses which are needed as "voorkennis" """
+        """Gives the mandatory courses which are needed as "voorkennis
+            return: list of mandatory "Voorkennis"" courses
+        """
         return_list = []
         for temp_course in self.bepaal_cursus_nog_niet_gedaan():
             return_list.append(temp_course.get_vereiste_voorkennis())
         return return_list
 
     def bepaal_cursus_gewenste_voorkennis(self) -> list[course]:
-        """Gives the requested courses which are needed as "voorkennis" """
+        """Gives the requested courses which are needed as "voorkennis"
+            return: list of "Gewenste voorkennis courses"
+        """
         return_list = []
         for temp_course in self.bepaal_cursus_nog_niet_gedaan():
             return_list.append(temp_course.get_gewenste_voorkennis())
         return return_list
 
     def bepaal_cursus_die_gedaan_kan_worden(self) -> list[course]:
-        """Gives the courses which can be done"""
+        """Gives the courses which can be done
+            return: list of courses that could be done
+        """
         return_list = []
         for temp_course in self.bepaal_cursus_nog_niet_gedaan():
             if set(temp_course.get_vereiste_voorkennis()).issubset(self.preparation.done_codes):
@@ -84,7 +95,9 @@ class Planner:
         return return_list
 
     def bepaal_cursus_vast_die_gedaan_kan_worden(self) -> list[course]:
-        """Gives the fixed courses which can be done"""
+        """Gives the fixed courses which can be done
+            return: list of fixed courses that could be done
+        """
         return_list = []
         for temp_course in self.bepaal_cursus_die_gedaan_kan_worden():
             if not temp_course.dates.nodates():
@@ -92,9 +105,48 @@ class Planner:
         return return_list
 
     def bepaal_cursus_variabel_die_gedaan_kan_worden(self) -> list[course]:
-        """Gives the variable courses which can be done"""
+        """Gives the variable courses which can be done
+            return: list of variable courses that could be done
+        """
         return_list = []
         for temp_course in self.bepaal_cursus_die_gedaan_kan_worden():
             if temp_course.dates.nodates():
                 return_list = return_list.append(course)
         return return_list
+
+############nog niet klaar #############33
+    def bepaal_cursus_in_quartaal(self,quarter) -> list[course]:
+        """Gives the courses with exams in this quarter
+                :param quartile: int that shows the quartile
+                :return: list of courses for this quartile
+        """
+        return_list = []
+        for temp_course in self.bepaal_cursus_die_gedaan_kan_worden():
+            if temp_course.dates.:
+                return_list = return_list.append(course)
+        return return_list
+
+
+    def make_string(self,course:course):
+        verplichte_voorkennis = []
+        gewenste_voorkennis = []
+
+        # get vereiset voorkennis
+        if not course.get_vereiste_voorkennis():
+            verplichte_voorkennis = 'geen verplichte voorkennis'
+        else:
+            verplichte_voorkennis = 'verplichte voorkennis: ' + str(course.get_vereiste_voorkennis())
+
+        # get gewenste voorkennis
+        if not course.get_gewenste_voorkennis():
+            gewenste_voorkennis += 'geen gewenste voorkennis'
+        else:
+            gewenste_voorkennis += 'gewenset voorkennis: ' + str(course.get_gewenste_voorkennis())
+
+        text = 'Kwartiel: ' + str(course.dates.quartile()) + ' \n' + \
+        'voorkennis: ' + self.preparation.done_codes + ' \n' + \
+        'Te volgen cursus: ' + course.code  + ', ' + course.title + ' \n' + \
+        verplichte_voorkennis + ' \n' + \
+        str(gewenste_voorkennis)
+
+        return text
